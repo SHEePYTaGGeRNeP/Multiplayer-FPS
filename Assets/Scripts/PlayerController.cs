75382a29e15d10564ee3a0b3445967d0f556e612
SHEePYTaGGeRNeP <sheepytaggernep@gmail.com>
2015-10-04 23:19:05 +0200

    Added prototyping + PlayerController

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    [RequireComponent(typeof(PlayerMotor))]
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField]
+        private float _speed = 5f;
+
+        private PlayerMotor _motor;
+
+        void Start()
+        {
+            this._motor = this.GetComponent<PlayerMotor>();
+        }
+
+        void Update()
+        {
+            // calculate movement velocity as a 3D vector.
+            float xMove = Input.GetAxisRaw("Horizontal");
+            float zMove = Input.GetAxisRaw("Vertical");
+
+            Vector3 moveHorizontal = this.transform.right * xMove;
+            Vector3 moveVertical = this.transform.forward * zMove;
+
+            Vector3 velocity = (moveHorizontal + moveVertical).normalized * this._speed * Time.deltaTime;
+
+            this._motor.Move(velocity);
+            
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class PlayerMotor : MonoBehaviour
+    {
+
+
+
+        public void Move(Vector3 velocity)
+        {
+            
+        }
+    }
+}
