48ba6a4eff6c7dc7102e583aa2688f51bc3650f1
SHEePYTaGGeRNeP <sheepytaggernep@gmail.com>
2015-10-11 22:59:40 +0200

    Video 3

---
+++ Assets/Scripts/PlayerSetup.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Assets.Scripts
+{
+    public class PlayerSetup : NetworkBehaviour
+    {
+        [SerializeField]
+        private Behaviour[] _componentsToDisable;
+
+        private Camera _sceneCamera;
+
+        void Start()
+        {
+            if (!this.isLocalPlayer)
+            {
+                foreach (Behaviour lvT in this._componentsToDisable)
+                    lvT.enabled = false;
+            }
+            else
+            {
+                this._sceneCamera = Camera.main;
+                if (this._sceneCamera != null) this._sceneCamera.gameObject.SetActive(false);
+                Camera.main.gameObject.SetActive(false);
+            }
+        }
+
+        void OnDisable()
+        {
+            if (this._sceneCamera != null)
+                this._sceneCamera.gameObject.SetActive(true);
+        }
+    }
+}
